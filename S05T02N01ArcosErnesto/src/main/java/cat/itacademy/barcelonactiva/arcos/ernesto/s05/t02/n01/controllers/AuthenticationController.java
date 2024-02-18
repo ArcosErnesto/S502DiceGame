7763a1716463459667b51703b315e354113d56c3
Ernesto Arcos <arcosernesto@gmail.com>
2024-02-18 12:52:27 +0100

    Añadida documentación swagger

--- S05T02N01ArcosErnesto/src/main/java/cat/itacademy/barcelonactiva/arcos/ernesto/s05/t02/n01/controllers/AuthenticationController.java
+++ S05T02N01ArcosErnesto/src/main/java/cat/itacademy/barcelonactiva/arcos/ernesto/s05/t02/n01/controllers/AuthenticationController.java
@@ -4,6 +4,7 @@ import cat.itacademy.barcelonactiva.arcos.ernesto.s05.t02.n01.model.dao.request.
 import cat.itacademy.barcelonactiva.arcos.ernesto.s05.t02.n01.model.dao.request.SigninRequest;
 import cat.itacademy.barcelonactiva.arcos.ernesto.s05.t02.n01.model.dao.response.JwtAuthenticationResponse;
 import cat.itacademy.barcelonactiva.arcos.ernesto.s05.t02.n01.model.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+
+    @Operation(summary = "SignUp new User")
     @PostMapping("/signup")
     public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
         return ResponseEntity.ok(authenticationService.signup(request));
     }
 
+    @Operation(summary = "SignIn User")
     @PostMapping("/signin")
     public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SigninRequest request) {
         return ResponseEntity.ok(authenticationService.signin(request));
